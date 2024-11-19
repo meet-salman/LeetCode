675b960b443f3119b826dd17e3b8e78fd3840a4c
Salman Ahmed <136422539+meet-salman@users.noreply.github.com>
2024-11-19 10:01:52 +0500

    Time: 0 ms (100%), Space: 12.4 MB (61.09%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
+        int idx = 0;
+
         for(int i = 0; i < nums.size(); i++){
 
             if(nums[i] >= target){
-                return i;
+                idx = i;
+                break;
             }
+            idx = i + 1;
         }
-        return nums.size();
+        return idx;
     }
 };
