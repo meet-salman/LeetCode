2214a719a6f78aab7c076a8b8a99f91cb7e57383
Salman Ahmed <136422539+meet-salman@users.noreply.github.com>
2024-11-28 13:26:53 +0500

    Time: 0 ms (100%), Space: 8.3 MB (12.35%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] < 91 && s[i] > 64) {
+
+                s[i] = 32 + s[i];
+            }
+        }
+
+        return s;
+    }
+};
