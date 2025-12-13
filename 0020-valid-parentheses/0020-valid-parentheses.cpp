6aa18ab5955a847435b9f13ac291718b9c7ad2dc
Salman Ahmed <136422539+meet-salman@users.noreply.github.com>
2025-12-13 08:58:45 -0800

    Time: 3 ms (9.43%), Space: 8.8 MB (66.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,26 +1,22 @@
+using st_ch = stack<char>;
+using map_ch = map<char, char>;
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        if (s.size() == 1 || s.size() % 2 != 0)
+            return false;
 
-        if (s.size() == 1)
-            return 0;
+        st_ch st;
+        map_ch mp = {{')', '('}, {'}', '{'}, {']', '['}};
 
-        for (auto& p : s) {
-            if (p == '(' || p == '[' || p == '{')
-                st.push(p);
-            else {
-                if (!st.empty()) {
-
-                    if (p == ')' && st.top() == '(' ||
-                        p == ']' && st.top() == '[' ||
-                        p == '}' && st.top() == '{')
-                        st.pop();
-                    else
-                        break;
-                } else
-                    st.push(p);
-            }
+        for (char& ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+            else if (!st.empty() && mp.at(ch) == st.top())
+                st.pop();
+            else
+                return false;
         }
         return (st.empty());
     }
