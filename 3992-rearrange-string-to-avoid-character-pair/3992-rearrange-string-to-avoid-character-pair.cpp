0d22a5f96e04849b6bc1899a40d4125b469b8a2c
Salman Ahmed <136422539+meet-salman@users.noreply.github.com>
2026-07-22 11:08:25 +0500

    Time: 4 ms (12.9%), Space: 9.2 MB (81.54%) - LeetHub

--- 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
@@ -3,14 +3,14 @@ public:
     string rearrangeString(string s, char x, char y) {
         string t = "";
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == y)
-                t+= s[i];
+        for(char c : s){
+            if(c == y)
+                t+= c;
         }
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] != y)
-                t+= s[i];
+        for(char c : s){
+            if(c != y)
+                t+= c;
         }
 
         return t;
